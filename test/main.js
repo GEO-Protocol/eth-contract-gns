bdb9d9627c919706190f10eb64cf5c3823e8cbbb
Svyatoslav Delukin <tarzeron@gmail.com>
2018-11-29 15:20:17 +0200

    Chore(test) access test

--- test/main.js
+++ test/main.js
@@ -1,39 +1,8 @@
 var GNS = artifacts.require("GNS");
 
-contract('GNS', function() {
+contract('GNS', function(accounts) {
 
-    /*
-    "name"
-    "name1"
-    "name2"
-    -------create valid---------
-    "name",['0x00','0x00','0x00','0x00','0x03','0x31','0x32','0x33']
-    "name",['0x00','0x00','0x00','0x00','0x03','0x31','0x2e','0x33']
-    "name",['0x00','0x00','0x00','0x00','0x03','0x71','0x42','0x33']
-    "name1",['0x00','0x00','0x00','0x00','0x03','0x71','0x42','0x33']
-    "name",['0x01','0xff','0xff','0xff','0xaa']
-    "name",['0x01','0xff','0xbb','0xff','0xaa']
-    "name",['0x02','0xff','0xbb','0xff','0xaa','0xff','0xbb','0xff','0xaa','0xff','0xbb','0xff','0xaa','0xff','0xbb','0xff','0xaa']
-    "name",['0x03','0x31','0x32','0x33']
-    "name",['0x03','0x31','0x33','0x33']
-    -------filter valid---------
-    "name",['0x00']
-    "name",['0x01']
-    "name",['0x02']
-    "name",['0x03']
-    "name1",['0x00']
-    "name2",['0x00']
-    ---------removing-----
-    "name",5
-    "name",['0x01','0xff','0xff','0xff','0xaa']
-    ---------invalid---------
-    "na.me",['0x00','0x00','0x00','0x00','0x03','0x31','0x32','0x33']
-    "name",['0x00','0x00','0x00','0x00','0x03','0x31','0x32']
-    "name",['0x01','0xff','0xff','0xaa']
-    "name",['0x02','0xff','0xbb','0xff','0xaa','0xff','0xbb','0xff','0xaa','0xff','0xbb','0xff','0xaa','0xff','0xbb','0xff']
-    "name",['0x03']
-    "name22",5
-    */
+    console.log("accounts",accounts);
 
     it("create, find, remove records", async () => {
         const gns = await GNS.deployed();
@@ -65,13 +34,41 @@ contract('GNS', function() {
         if(!passed){
             throw "createRecord can create record not unique";
         }
+
+        assert.equal(await gns.getRecordsCount("name1"), 1, "getRecordsCount wrong result");
     });
 
     it("access test", async () => {
-    });
+        const gns = await GNS.deployed();
 
+        await gns.createRecord("access0",['0x00','0x00','0x00','0x00','0x03','0x31','0x32','0x33'],{from:accounts[0]});
+        await gns.createRecord("access1",['0x00','0x00','0x00','0x00','0x03','0x31','0x32','0x33'],{from:accounts[1]});
 
-    it("Amount of gas used", async () => {
+        // account1 get count records of account0
+        await gns.getRecordsCount("name",{from:accounts[1]});
+
+        {
+            let passed = true;
+            try {
+                await gns.createRecord("access0",['0x00','0x00','0x00','0x00','0x03','0x31','0x32','0x33'],{from:accounts[1]});
+                passed=false;
+            }catch (_) {}
+            if(!passed){
+                throw "createRecord not owner have access";
+            }
+        }
+
+        {
+            let passed = true;
+            try {
+                const rawRecordAt = await gns.getRawRecordAt("name",{from:accounts[1]});
+                await gns.removeRecord("name",rawRecordAt);
+                passed=false;
+            }catch (_) {}
+            if(!passed){
+                throw "createRecord not owner have access";
+            }
+        }
     });
 
 });
